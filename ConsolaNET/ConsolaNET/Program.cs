e1c8cddfb7b1c216c6d07ea60dd5e27323aef79b
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-03-26 18:03:05 +0100

    Escritura de código C# por primera vez

--- ConsolaNET/ConsolaNET/Program.cs
+++ ConsolaNET/ConsolaNET/Program.cs
@@ -20,6 +20,20 @@ namespace ConsolaNET
             Console.Write("segunda");
             Console.Write(" ");
             Console.Write("linea.");
+
+            /**
+             * Test
+             * 
+             * 1. ¿Cuál es el principal trabajo del compilador?
+             * El compilador principalmente convierte el código a un formato ejecutable que el equipo pueda entender.
+             * 
+             * 2. ¿Cuál de las siguientes afirmaciones sobre C# es verdadera?
+             * Hay que usar comillas dobles para crear una cadena literal.
+             * 
+             * 3. ¿Qué problema hay en esta línea de código? Console.WriteLine("What is wrong with me?")
+             * Falta un punto y coma al final.
+             * 
+             */
         }
     }
 }
