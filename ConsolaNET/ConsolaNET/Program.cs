b3247fc5f2ea8e293959c07404842039d5d0c3d3
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-03-26 19:40:50 +0100

    Llamar a métodos desde la biblioteca de clases .NET mediante C#

--- ConsolaNET/ConsolaNET/Program.cs
+++ ConsolaNET/ConsolaNET/Program.cs
@@ -7,19 +7,24 @@ namespace ConsolaNET
         static void Main(string[] args)
         {
             //Desafio
-            int fahrenheit = 94;
-            decimal celsius = (fahrenheit - 32) * (5 / 9.0m);
+            int primerValor = 500;
+            int segundoValor = 600;
+            int mayor;
 
-            Console.WriteLine("La temperatura es de " + celsius + " Celsius");
+            mayor = Math.Max(primerValor, segundoValor);
+            Console.WriteLine("Es mayor el número "+mayor);
 
             /**
              * Test
              * 
-             * 1. ¿Cuál será el resultado de la siguiente línea de código? Console.WriteLine(5 / 10);
-             * 0
+             * 1. ¿Qué es un objeto?
+             * Una instancia de una clase
              * 
-             * 2. ¿Cuál de las siguientes líneas de código usa el operador de asignación de suma?
-             * value += 5;
+             * 2. ¿Cuál de las siguientes afirmaciones es verdadera?
+             * La biblioteca de clases .NET contiene definiciones de tipos de datos que se utilizan en C#.
+             * 
+             * 3. ¿Qué símbolo es el operador de invocación de método, que realmente ejecuta un método?
+             * ()
              * 
              */
         }
