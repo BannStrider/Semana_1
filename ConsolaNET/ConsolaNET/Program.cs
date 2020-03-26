80cad27ca8ae1405739ad110f1375645ee63b2d8
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-03-26 19:04:34 +0100

    Aplicación de formato de cadena básico en C#

--- ConsolaNET/ConsolaNET/Program.cs
+++ ConsolaNET/ConsolaNET/Program.cs
@@ -7,25 +7,29 @@ namespace ConsolaNET
         static void Main(string[] args)
         {
             //Desafio
-            String nombre = "Bob";
-            int numero = 3;
-            decimal temperatura = 34.4m;
+            string nombreProyecto = "ACME";
 
-            Console.Write("¡Hola, " + nombre + "! ");
-            Console.Write("Tienes " + numero + " mensajes en tu buzón. ");
-            Console.Write("La temperatura actual es de " + temperatura + " celsius.");
+            string mensaje = "Ver salida español";
+            string ruta = $@"C:\Ejemplo\{nombreProyecto}\es_ES\";
+
+            string mensajeRuso = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+            string rutaRuso = $@"C:\Ejemplo\{nombreProyecto}\ru_RU\";
+
+            Console.WriteLine(mensaje + ": \n" + ruta + "\n\n");
+
+            Console.WriteLine(mensajeRuso + ": \n" + rutaRuso);
 
             /**
              * Test
              * 
-             * 1. ¿Cuáles de los siguientes nombres de variable se deben evitar?
-             * $DATA
+             * 1. ¿Cuál de las siguientes secuencias de escape usaría para agregar comillas dobles a una cadena literal?
+             * \"
              * 
-             * 2. Cuál es el problema de esta línea de código: var message;
-             * Uso de la palabra clave var sin inicializar la variable
+             * 2. ¿Cuál de las siguientes líneas de código usa correctamente la interpolación de cadenas, suponiendo que la variable value es una cadena?
+             * Console.WriteLine($"My value: {value}");
              * 
-             * 3. Cuál de las siguientes líneas de código crea una variable correctamente.
-             * decimal x = 12.3m;
+             * 3. Ha insertado caracteres Unicode en las cadenas para presentar un mensaje especial en tailandés, pero el mensaje solo se muestra como signos de interrogación. ¿Qué ha ocurrido?
+             * La consola del usuario no admite caracteres Unicode.
              * 
              */
         }
