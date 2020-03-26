73d2de447d52b181a8259517bb4992081d24a78a
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-03-26 17:53:26 +0100

    Escritura de código de C# por primera vez

--- ConsolaNET/ConsolaNET/Program.cs
+++ ConsolaNET/ConsolaNET/Program.cs
@@ -6,7 +6,20 @@ namespace ConsolaNET
     {
         static void Main(string[] args)
         {
+            //Primeros pasos
             Console.WriteLine("¡Hola Mundo!");
+
+            //Desafio
+            Console.WriteLine("Esta es la primera linea.");
+            Console.Write("Esta");
+            Console.Write(" ");
+            Console.Write("es");
+            Console.Write(" ");
+            Console.Write("la");
+            Console.Write(" ");
+            Console.Write("segunda");
+            Console.Write(" ");
+            Console.Write("linea.");
         }
     }
 }
