58a290bf4111c644c94426bb4257fcb5aa68f521
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-03-26 19:20:08 +0100

    Realización de operaciones básicas con números en C#

--- ConsolaNET/ConsolaNET/Program.cs
+++ ConsolaNET/ConsolaNET/Program.cs
@@ -7,29 +7,19 @@ namespace ConsolaNET
         static void Main(string[] args)
         {
             //Desafio
-            string nombreProyecto = "ACME";
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32) * (5 / 9.0m);
 
-            string mensaje = "Ver salida español";
-            string ruta = $@"C:\Ejemplo\{nombreProyecto}\es_ES\";
-
-            string mensajeRuso = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-            string rutaRuso = $@"C:\Ejemplo\{nombreProyecto}\ru_RU\";
-
-            Console.WriteLine(mensaje + ": \n" + ruta + "\n\n");
-
-            Console.WriteLine(mensajeRuso + ": \n" + rutaRuso);
+            Console.WriteLine("La temperatura es de " + celsius + " Celsius");
 
             /**
              * Test
              * 
-             * 1. ¿Cuál de las siguientes secuencias de escape usaría para agregar comillas dobles a una cadena literal?
-             * \"
+             * 1. ¿Cuál será el resultado de la siguiente línea de código? Console.WriteLine(5 / 10);
+             * 0
              * 
-             * 2. ¿Cuál de las siguientes líneas de código usa correctamente la interpolación de cadenas, suponiendo que la variable value es una cadena?
-             * Console.WriteLine($"My value: {value}");
-             * 
-             * 3. Ha insertado caracteres Unicode en las cadenas para presentar un mensaje especial en tailandés, pero el mensaje solo se muestra como signos de interrogación. ¿Qué ha ocurrido?
-             * La consola del usuario no admite caracteres Unicode.
+             * 2. ¿Cuál de las siguientes líneas de código usa el operador de asignación de suma?
+             * value += 5;
              * 
              */
         }
