c9be8dbd77e588acb58bdc9df68b296bb2d617a3
Oscar Gutierrez Garcia <oskiyas94@hotmail.es>
2020-03-26 20:25:39 +0100

    Cómo agregar la lógica de decisión al código mediante la instrucción “if-elseif-else” en C#

--- ConsolaNET/ConsolaNET/Program.cs
+++ ConsolaNET/ConsolaNET/Program.cs
@@ -7,24 +7,45 @@ namespace ConsolaNET
         static void Main(string[] args)
         {
             //Desafio
-            int primerValor = 500;
-            int segundoValor = 600;
-            int mayor;
+            Random random = new Random();
+            int diasRestantes = random.Next(12);
+            int porcentajeDescuento = 0;
 
-            mayor = Math.Max(primerValor, segundoValor);
-            Console.WriteLine("Es mayor el número "+mayor);
+            if (diasRestantes == 0)
+            {
+                Console.WriteLine("Tu suscripción ha caducado.");
+            }
+            else if (diasRestantes == 1)
+            {
+                Console.WriteLine("Tu suscripción caduca en horas.");
+                porcentajeDescuento = 20;
+            }
+            else if (diasRestantes <= 5)
+            {
+                Console.WriteLine($"Tu suscripción caduca en {diasRestantes} días.");
+                porcentajeDescuento = 10;
+            }
+            else if (diasRestantes <= 10)
+            {
+                Console.WriteLine("Tu suscripción caduca pronto. ¡Renuevala ahora!");
+            }
+
+            if (porcentajeDescuento > 0)
+            {
+                Console.WriteLine($"¡Renueva ahora y ahorra {porcentajeDescuento}%!");
+            }
 
             /**
              * Test
              * 
-             * 1. ¿Qué es un objeto?
-             * Una instancia de una clase
+             * 1. Si int x = 5;, ¿cuál de las siguientes expresiones booleana es válida y se evalúa como verdadera?
+             * x == 5
              * 
-             * 2. ¿Cuál de las siguientes afirmaciones es verdadera?
-             * La biblioteca de clases .NET contiene definiciones de tipos de datos que se utilizan en C#.
+             * 2. ¿Cuál de las siguientes opciones no es un operador válido?
+             * %%
              * 
-             * 3. ¿Qué símbolo es el operador de invocación de método, que realmente ejecuta un método?
-             * ()
+             * 3. ¿Cuál de las siguientes opciones sobre la instrucción if es falsa?
+             * Puede agregar else antes de una instrucción else if.
              * 
              */
         }
